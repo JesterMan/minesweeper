678d6dbde307a82de2ede2f79c0f2313dfd5cfd4
JesterMan <seanglang@gmail.com>
2014-01-05 21:39:13 -0800

    tables

--- Minesweeper.js
+++ Minesweeper.js
@@ -1,13 +1,33 @@
 board = []
-var boardCount =8;
+var boardCount =8,
+ 	table = '<table>';
 for (var i = 0; i < boardCount; i++) {
-	board.push([])
+	board.push([]);
+	 table+="<tr>";
 	for (var x =0; x < boardCount; x++) {
-		board[i].push(new tile())
+		board[i].push(new tile());
+    	table+='<td></td>';
 	};
+	table+="</tr>";
+	
 };
+table+="</table>"
+debugger;
+$(".game").html(table);
 
 function tile() {
 	this.beenClicked = false;
 	this.hasBomb = false;
 }
+
+for (var i = 0; i < 10;) {
+	var x = Math.floor((Math.random()*7)+1),
+		y = Math.floor((Math.random()*7)+1);
+		if (board[x][y].hasBomb==false) {
+			board[x][y].hasBomb=true;
+			i++;
+		}
+};
+
+
+	
