1930a6c6d7962476124cc929278881b56fd3311a
JesterMan <seanglang@gmail.com>
2014-01-12 20:24:07 -0800

    add godmode and other features

--- Minesweeper.js
+++ Minesweeper.js
@@ -12,6 +12,8 @@ var boardCount =8,
 function newBoard() {
     correctMark=0;
     markedBomb=0;
+    boardCount = document.getElementById('boardSize').value;
+    bombCount = document.getElementById('bombin').value;
     $('#gameBoard').remove();
     for (var i = 0; i < boardCount; i++) {
         board.push([]);
@@ -89,13 +91,18 @@ $(document).ready (function(){
             alert("not quite")
         }
     });
+    $("#powerOverwhelming").click(function(){
+        for (var i = 0; i <bombSet.length; i++) {
+            $("tr:eq("+bombSet[i][0]+") td:eq("+bombSet[i][1]+")").text("O").addClass('revealed');
+        };
+    });
+
 });
 
 
 function clickTile(row,col) {        
     if (board[row][col].hasBomb){
-        $("tr:eq("+row+") td:eq("+col+")").text("X");
-        $("tr:eq("+row+") td:eq("+col+")").addClass('Bomb');
+        $("tr:eq("+row+") td:eq("+col+")").text("X").addClass('Bomb');
         $("td").unbind('click');
         alert("Boom! Game over.")
     }
@@ -103,14 +110,12 @@ function clickTile(row,col) {
         board[row][col].beenClicked=true;
         mineCount=countMine(row,col);
         if (mineCount==0){
-            $("tr:eq("+row+") td:eq("+col+")").addClass('known');
-            $("tr:eq("+row+") td:eq("+col+")").text("");
+            $("tr:eq("+row+") td:eq("+col+")").addClass('known').text("");
             testNeighbors(row,col);
             //run more tests
         }
         else {
-            $("tr:eq("+row+") td:eq("+col+")").text(mineCount);
-            $("tr:eq("+row+") td:eq("+col+")").addClass('known');
+            $("tr:eq("+row+") td:eq("+col+")").text(mineCount).addClass('known');
         }
     }        
 };
